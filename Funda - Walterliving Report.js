776b7a49342d3e640301a76d9a6c450b45fbf654
L <rankjie@gmail.com>
2023-04-25 12:59:59 +0200

    Create Funda - Walterliving Report.js

---
+++ Funda - Walterliving Report.js
@@ -0,0 +1,122 @@
+// ==UserScript==
+// @name         Funda - Walterliving Report
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Grab info from Walterliving.
+// @author       Beexio BV
+// @match        *://www.funda.nl/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=walterliving.com
+// @grant        GM_xmlhttpRequest
+// @connect      api.walterliving.com
+// ==/UserScript==
+
+(async function() {
+    'use strict';
+    const listing_title_regexp = /.*:\ (?<address>.*)\ (?<zipcode>\d{4}\ [A-Z]{2})\ (?<city>.*)\ \[funda\]$/;
+    const listing_url_regexp = /https:\/\/www\.funda\.nl\/koop\/.*\/.*\/$/;
+    function fetchListingInfo() {
+        const found = document.title.match(listing_title_regexp);
+        let data
+        if (found && found.groups && found.groups.zipcode && found.groups.address) {
+            data = JSON.stringify({
+                "url": location.href,
+                "address": found.groups.address,
+                "zipcode": found.groups.zipcode
+            });
+        } else {
+            data = JSON.stringify({
+                "url": location.href
+            });
+        }
+        return (new Promise((resolve, reject) => {
+            GM_xmlhttpRequest({
+                url: 'https://api.walterliving.com/hunter/lookup',
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                data: data,
+                onload: (r) => {
+                    resolve(r.responseText)
+                }
+            })
+        })).then(text => JSON.parse(text))
+    }
+    const ret = await fetchListingInfo()
+    console.log('ret:', ret)
+    if (ret.status !== 'ok') {
+        return
+    }
+    const wozs = ret.changes.filter(el => {
+        console.log(el)
+        return el.source === 'WOZ'
+    })
+    const askingPrices = ret.changes.filter(el => el.status === "Vraagprijs")
+    const wozString = genString(wozs)
+    const askingString = genString(askingPrices)
+    displaySpeed(ret)
+    function displaySpeed(infoObj) {
+        document.getElementsByClassName('object-header__pricing')[0].insertAdjacentHTML('beforebegin', `
+    <div style="display:flex">
+      <div style="flex: 0 0 50%;">
+        <p style="font-size: 12px">
+          <b>Asking:</b>
+          <br>
+          ${askingString}
+        </p>
+        <p><a href="${ret.report.url}" target="_blank">Walter Report</a></p>
+      </div>
+      <div style="flex: 0 0 50%;">
+        <p style="font-size: 12px">
+          <b>WOZ:</b>
+          <br>
+          ${wozString}
+        </p>
+      </div>
+    </div>`)
+    }
+
+    function genString(arr) {
+        const ret = []
+        let last = null
+        for (const el of arr.reverse()) {
+            const price = shortPrice(el.price)
+            let str = `${el.date}: ${el.human_price.slice(0, 1)}${price.num}${price.metric}`
+            if (last) {
+                const change = el.price - last.price
+                const changeShort = shortPrice(change)
+                const changePct = Math.ceil(change / last.price * 100)
+                let color = `#7dd321`
+                if (change < 0) {
+                    color = `#e67e22`
+                }
+                const colorStyle = `color: ${color}`
+                if (change) {
+                    str += `<span style="${colorStyle};margin-left: 5px">${changeShort.num}${changeShort.metric} (${changePct}%)</span>`
+                }
+            }
+            last = el
+            ret.push(str)
+        }
+        return ret.reverse().join('<br>')
+    }
+
+    function shortPrice(price) {
+        const m = Math.ceil(price / 100000) / 10
+        if (m < 1) {
+            return {num: Math.ceil(price / 1000) / 10, metric: '万'}
+        }
+        return {num: m, metric: 'm'}
+    }
+
+    function genWozString(wozs) {
+        const ret = []
+        for (const woz of wozs) {
+            console.log('processing woz:', woz)
+            const price = shortPrice(woz.human_price)
+            console.log(price)
+            ret.push(`${woz.date}: ${price.num}${price.metric}`)
+        }
+        return ret.join('<br>')
+    }
+})();
