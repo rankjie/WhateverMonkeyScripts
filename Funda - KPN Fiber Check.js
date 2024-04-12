58886e4ad63b0cd7c0e1bb4455a4fad29435f81f
L <rankjie@gmail.com>
2024-04-12 21:24:15 +0200

    update for new funda beta

--- Funda - KPN Fiber Check.js
+++ Funda - KPN Fiber Check.js
@@ -1,115 +1,191 @@
 // ==UserScript==
 // @name         Funda KPN Fiber Check
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Get KPN Fiber status and Internet speed.
 // @author       Beexio BV
 // @match        *://www.funda.nl/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kpn.com
 // @grant        GM_xmlhttpRequest
 // @connect      api-prd.kpn.com
+// @require https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
 (async function() {
     'use strict';
-    const KPN_API_CLIENT_ID = ""
-    const KPN_API_CLIENT_SECRET = ""
-    const KPN_TOKEN_KEY = "KPN_TOKEN_OBJ"
-    const address = document.getElementsByClassName('object-header__title')?.[0]?.textContent
-    if (!address) {
-        console.log('can not parse address')
-        return
-    }
-    const addressArr = address.split(/\s+/)
-    let houseNumber = addressArr[addressArr.length - 1]
-    let ext = ""
-    if (!Number(houseNumber) || !isNaN(Number(addressArr[addressArr.length - 2]))) {
-        ext = houseNumber
-        houseNumber = addressArr[addressArr.length - 2]
-    }
-    const zipCode = document.getElementsByClassName('object-header__subtitle')?.[0]?.textContent?.split(/\s+/)?.slice(0, 2).join('')
 
-    const {access_token} = await getToken()
-    console.log('access_token', access_token)
-    console.log(address)
-    const ret = await getSpeed(access_token, houseNumber, zipCode, ext)
-    console.log('ret:', ret)
-    displaySpeed(ret)
+    const actualFuction = async () => {
+        const sleep = async (t) => {
+            return new Promise((resolve, reject) => {
+                setTimeout(resolve, t)
+            })
+        }
+        // await sleep(1 * 1000);
 
-    function displaySpeed(infoObj) {
-        document.getElementsByClassName('object-header__pricing')[0].insertAdjacentHTML('afterEnd', `
+        let ret
+        const KPN_TOKEN_KEY = "KPN_TOKEN_OBJ"
+
+        const getTargetElement = () => {
+            let ret = document.getElementsByClassName('object-header__pricing')[0]
+            if (location.href.includes('/detail/')) {
+                ret = document.getElementsByClassName('object-header__container')[0].parentElement.parentElement
+            }
+            const newElement = document.createElement("div")
+            ret.appendChild(newElement)
+            return newElement
+        }
+
+        let targetElement = getTargetElement()
+        let houseNumber
+        let zipCode
+        let newFunda
+        let addressArr
+
+        let ext = ""
+
+
+        console.log('location.href is', location.href)
+        // new version of funda
+        if (location.href.includes('/detail/')) {
+            console.log('new funda');
+            newFunda = true;
+            addressArr = document.getElementsByClassName('object-header__container')[0].firstChild.textContent?.split(/\s+/)
+            houseNumber = document.getElementsByClassName('object-header__container')[0].firstChild.textContent?.split(/\s+/).pop()
+            zipCode = document.getElementsByClassName('object-header__container')[0].children[1].textContent?.split(/\s+/)?.slice(0, 2).join('')
+        }
+
+
+
+        main()
+
+        async function main() {
+            showInfo('Fetching KPN Fiber Info...')
+
+            try {
+                if (!newFunda) {
+                    const address = document.getElementsByClassName('object-header__title')?.[0]?.textContent
+                    if (!address) {
+                        console.log('can not parse address')
+                        return
+                    }
+                    addressArr = address.split(/\s+/)
+                    houseNumber = addressArr[addressArr.length - 1]
+                    zipCode = document.getElementsByClassName('object-header__subtitle')?.[0]?.textContent?.split(/\s+/)?.slice(0, 2).join('')
+                }
+                if (!Number(houseNumber) || !isNaN(Number(addressArr[addressArr.length - 2]))) {
+                    ext = houseNumber
+                    houseNumber = addressArr[addressArr.length - 2]
+                }
+                const {access_token} = await getToken()
+                console.log('access_token', access_token)
+
+                ret = await getSpeed(access_token, houseNumber, zipCode, ext)
+                console.log('ret:', ret)
+                if (ret.error) {
+                    throw ret
+                }
+                showInfo()
+                displaySpeed(ret)
+            } catch (e) {
+                console.log('kpn err:', e)
+                showInfo()
+                showInfo('Fetch KPN Fiber Info Failed:' + JSON.stringify(e.error))
+            }
+        }
+
+        function displaySpeed(infoObj) {
+            getTargetElement().insertAdjacentHTML('afterEnd', `
     <div class="object-header__pricing fd-text-size-l fd-flex--bp-m fd-align-items-center" style="font-size: 1rem" id="kpn-info">
-      <div>
-        <p class="object-header__price" style="font-size: 1rem">KPN Fiber: ${infoObj.fixed_info.fiber_access ? '✅' : '❌'} | ⬇️ ${infoObj.bandwidth.down || '-'}Mbps / ⬆️ ${infoObj.bandwidth.up || '-'}Mbps</strong>
-      </div>
+      <p class="object-header__price" style="font-size: 1rem;white-space: initial;">KPN Fiber: ${infoObj.fixed_info.fiber_access ? '✅' : '❌'} | ⬇️ ${infoObj.bandwidth.down || '-'}Mbps / ⬆️ ${infoObj.bandwidth.up || '-'}Mbps</p>
     </div>
 
     <details>
-      <summary>KPN Details</summary>
+      <summary style="cursor:pointer">KPN Details</summary>
       <pre style="font-size:8px">${JSON.stringify(ret, null, 2)}</pre>
     </details>`)
-    }
-
-    async function getSpeed(accessToken, houseNumber, zipCode, ext) {
-        console.log(`houseNumber, zipCode, ext`, houseNumber, zipCode, ext)
-        return (new Promise((resolve, reject) => {
-            GM_xmlhttpRequest({
-                url: 'https://api-prd.kpn.com/network/kpn/internet-speed-check/offer',
-                method: 'POST',
-                headers:{
-                    'Authorization': `Bearer ${accessToken}`,
-                    'Content-Type': 'application/json'
-                },
-                data: JSON.stringify({
-                    service_address: {
-                        "house_number": Number(houseNumber),
-                        "zip_code": zipCode,
-                        "house_number_extension": ext
-                    }
-                }),
-                onload: (r) => {
-                    resolve(r.responseText)
-                }
-            })
-        })).then(text => JSON.parse(text))
-    }
-
-    async function getToken(){
-        const tokenObj = localStorage.getItem(KPN_TOKEN_KEY)
-        console.log("cached token", tokenObj)
-        if (tokenObj) {
-            console.log("use cached token")
-            const obj = JSON.parse(tokenObj)
-            const expireT = Number(obj.issued_at) / 1000 + Number(obj.expires_in)
-            const tToExpire = expireT - new Date() / 1000
-            console.log(tToExpire, "seconds to expire")
-            if (tToExpire <= 60) {
-                console.log("cached token expired.")
-            } else {
-                return JSON.parse(tokenObj)
-            }
         }
-        return (new Promise((resolve, reject) => {
-            const data = new URLSearchParams({
-                    client_id: KPN_API_CLIENT_ID,
-                    client_secret: KPN_API_CLIENT_SECRET
+
+        function showInfo(str) {
+            if (!str) {
+                return document.getElementById('kpn-working')?.remove?.()
+            }
+            console.log('showing info:', str)
+            getTargetElement().insertAdjacentHTML('afterEnd', `
+    <div id="kpn-working">
+        <p style="font-size: 1rem;white-space: initial;">${str || 'Fetching KPN Fiber Info...'}</p>
+        <a onclick="main()">Re-try</a>
+    </div>`)
+        }
+
+        async function getSpeed(accessToken, houseNumber, zipCode, ext) {
+            console.log(`houseNumber, zipCode, ext`, houseNumber, zipCode, ext)
+            return (new Promise((resolve, reject) => {
+                GM_xmlhttpRequest({
+                    url: 'https://api-prd.kpn.com/network/kpn/internet-speed-check/offer',
+                    method: 'POST',
+                    headers:{
+                        'Authorization': `Bearer ${accessToken}`,
+                        'Content-Type': 'application/json'
+                    },
+                    data: JSON.stringify({
+                        service_address: {
+                            "house_number": Number(houseNumber),
+                            "zip_code": zipCode,
+                            "house_number_extension": ext
+                        }
+                    }),
+                    onload: (r) => {
+                        resolve(r.responseText)
+                    }
                 })
-            console.log('post data:', data, data.toString())
-            GM_xmlhttpRequest({
-                url: 'https://api-prd.kpn.com/oauth/client_credential/accesstoken?grant_type=client_credentials',
-                method: 'POST',
-                headers:{
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                data: data.toString(),
-                onload: (r) => {
-                    console.log('response:', r.responseText)
-                    resolve(r.responseText)
+            })).then(text => JSON.parse(text))
+        }
+
+        async function getToken(){
+            const tokenObj = localStorage.getItem(KPN_TOKEN_KEY)
+            console.log("cached token", tokenObj)
+            if (tokenObj) {
+                console.log("use cached token")
+                const obj = JSON.parse(tokenObj)
+                const expireT = Number(obj.issued_at) / 1000 + Number(obj.expires_in)
+                const tToExpire = expireT - new Date() / 1000
+                console.log(tToExpire, "seconds to expire")
+                if (tToExpire <= 60) {
+                    console.log("cached token expired.")
+                } else {
+                    return JSON.parse(tokenObj)
                 }
+            }
+            return (new Promise((resolve, reject) => {
+                const data = new URLSearchParams({
+                    client_id: "<REPLACE_WITH_YOUR_OWN",
+                    client_secret: "<REPLACE_WITH_YOUR_OWN>"
+                })
+                console.log('post data:', data, data.toString())
+                GM_xmlhttpRequest({
+                    url: 'https://api-prd.kpn.com/oauth/client_credential/accesstoken?grant_type=client_credentials',
+                    method: 'POST',
+                    headers:{
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    data: data.toString(),
+                    onload: (r) => {
+                        console.log('response:', r.responseText)
+                        resolve(r.responseText)
+                    }
+                })
+            })).then(text => {
+                console.log('text:', text)
+                return JSON.parse(text)
             })
-        })).then(text => {
-            console.log('text:', text)
-            return JSON.parse(text)
-        })
+        }
     }
+
+    const interval = setInterval(() => {
+        console.log(document.readyState)
+        if (document.readyState === 'complete') {
+            actualFuction()
+            clearInterval(interval)
+        }
+    }, 500)
 })();
